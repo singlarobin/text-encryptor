c336b648841e579b7d983dfd3c446e6bd44e019e
singlarobin <singlarobin99@gmail.com>
2021-06-06 13:13:29 +0530

    selectOptions is converted to object from array

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -6,17 +6,25 @@ Minim ut commodo exercitation deserunt sunt qui proident culpa ad ex tempor aliq
 Et ullamco deserunt minim adipisicing et est dolor.
 `;
 
-const selectOptions = Object.freeze([
-    { 'name': '15 Minutes', 'value': '15' },
-    { 'name': '30 Minutes', 'value': '30' },
-    { 'name': '1 Hour', 'value': '60' },
-    { 'name': '3 Hour', 'value': '180' },
-    { 'name': '6 Hour', 'value': '360' },
-    { 'name': '12 Hour', 'value': '720' },
-    { 'name': '1 Day', 'value':  '1440' }
-]) ; 
+const selectOptions = Object.freeze({
+    '15_MIN' : { 'name': '15 Minutes', 'value': '15' }, //Value is in minutes
+    '30_MIN' : { 'name': '30 Minutes', 'value': '30' },
+    '1_HOUR' : { 'name': '1 Hour', 'value': '60' },
+    '3_HOUR' : { 'name': '3 Hour', 'value': '180' },
+    '6_HOUR' : { 'name': '6 Hour', 'value': '360' },
+    '12_HOUR' : { 'name': '12 Hour', 'value': '720' },
+    '1_DAY' : { 'name': '1 Day', 'value':  '1440' }
+});
+
+const severityOptions = Object.freeze({
+    INFO: 1,
+    WARNING: 2,
+    ERROR: 3,
+    SUCCESS: 4
+});
 
 export {
     descriptionValue,
-    selectOptions
-}
+    selectOptions,
+    severityOptions
+};
