a570948999339a58d614829881d9510083363555
singlarobin <singlarobin99@gmail.com>
2021-06-22 00:03:23 +0530

    Error Component added!

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -1,8 +1,18 @@
+import { Fragment, useCallback } from 'react';
 import classes from './styles.module.css';
+import Button from '../button';
 
 const Error = props => {
     const { message } = props;
-    return <p className={classes.errorContent}>{message}</p>;
+    const handleRedirectToHome = useCallback(() => props.history.push('/'));
+    
+    return <Fragment>
+        <p className={classes.errorContent}>{message}</p>
+        <Button onClick={handleRedirectToHome} 
+            style={{ margin: '1rem', padding: '0.5rem 0.75rem' }}>Create New Message</Button>
+    </Fragment>;
 };
 
 export default Error;
+
+Error.defaultProps = { message: '404 Page Not Found' };
