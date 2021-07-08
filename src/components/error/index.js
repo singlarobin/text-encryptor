d286a9896e39de742c7a12f5e3b2382902cb8bcf
singlarobin <singlarobin99@gmail.com>
2021-07-08 23:13:29 +0530

    requested changes done!

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -4,16 +4,21 @@ import Button from '../button';
 
 const Error = props => {
     const { message, status, buttonLabel, onClick } = props;
-    
+
     return <Fragment>
         <div className={classes.errorContainer}>
             <div className={classes.errorStatus}>{status}</div>
-            <div className={classes.errorContent}>{message}
-                <br/> Click on the button below to create a message.</div>
+            <div className={classes.errorContent}>
+                {message}
+                <br />
+                Click on the button below to create a message.
+            </div>
         </div>
-        {typeof(onClick) === 'function'&&  
-        <Button onClick={onClick} 
-            style={{ margin: '0rem auto', padding: '0.5rem 0.75rem' }}>{buttonLabel}</Button>}
+        {typeof onClick === 'function' &&
+            <Button onClick={onClick}
+                style={{ margin: '0rem auto', padding: '0.5rem 0.75rem' }}>
+                {buttonLabel}
+            </Button>}
     </Fragment>;
 };
 
