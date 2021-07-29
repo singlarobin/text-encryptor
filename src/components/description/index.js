c6b47526258a034247aedf03425a673816954f2a
Rohan Gupta <guptarohan369@gmail.com>
2021-07-30 00:00:36 +0530

    Resize description

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -1,6 +1,5 @@
 import classes from './styles.module.css';
 import { DESCRIPTION_VALUE } from '../constants';
-import IconButton from '../iconButton';
 import ExpandIcon from '../../assests/expandIcon';
 import { useState } from 'react';
 import { themed } from '../../utils/theme';
@@ -11,15 +10,18 @@ const Description = () => {
     const handleShowContent = () => setShowContent(!showContent);
 
     return <div className={classes.descriptionContainer}>
-        <div className={classes.descriptionHeading}>
+        <div className={classes.descriptionHeading} onClick={handleShowContent}>
             <div className={classes.descriptionLabel}>How it Works?</div>
-            <IconButton onClick={handleShowContent} style={{ filter: 'brightness(90%)' }}>
-                <ExpandIcon strokeColor={themed('#148077', '#36b6ab')}
-                    style={{
-                        transform: showContent ? 'rotate(180deg)' : 'rotate(0)',
-                        transition: 'transform 0.15s'
-                    }} />
-            </IconButton>
+            <ExpandIcon
+                height='0.75rem'
+                width='0.75rem'
+                strokeColor={themed('#1480779f', '#36b6aba7')}
+                style={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    transform: showContent ? 'rotate(180deg)' : 'rotate(0)',
+                    transition: 'transform 0.15s'
+                }}
+            />
         </div>
         {showContent && <p className={classes.descriptionContent}>{DESCRIPTION_VALUE}</p>}
     </div>;
